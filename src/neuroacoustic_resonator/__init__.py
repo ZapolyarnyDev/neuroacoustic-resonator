959bba923fb7a19758b2327c6a6bce45e8830f2a
Zapolyarny <zapolyarnynorth@gmail.com>
2026-05-07 21:49:15 +0300

    chore: adopt src package layout

--- src/neuroacoustic_resonator/__init__.py
+++ src/neuroacoustic_resonator/__init__.py
@@ -1,5 +1,5 @@
 """Core package for the neuroacoustic resonator."""
 
-from neuroacoustic_resonator.field import FieldConfig, OscillatorField, FieldState
+from neuroacoustic_resonator.field import FieldConfig, FieldState, OscillatorField
 
 __all__ = ["FieldConfig", "FieldState", "OscillatorField"]
